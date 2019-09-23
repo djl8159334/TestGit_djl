03a489eb45033d012a7fdd078dca5bc69d77b88d
djl2_glb <ASUS@192.168.2.165>
2019-09-23 09:51:26 +0800

    change teacher

--- src/com/djl/entity/Teacher.java
+++ src/com/djl/entity/Teacher.java
@@ -2,10 +2,7 @@ package com.djl.entity;
 
 public class Teacher {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("我是老师哈哈。。");
-=======
-		System.out.println("我是老师吼吼");
->>>>>>> branch 'master' of https://github.com/djl8159334/TestGit_djl.git
+		System.out.println("我是老师吼吼。。");
 	}
 }
